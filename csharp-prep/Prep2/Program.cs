ef35b6c561c63ccf777c648233dbae530d214d8a
kolawole1248 <kolawoleuthman12@gmail.com>
2024-01-13 00:52:48 +0100

    Finished prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your percentage");
+        string answer=Console.ReadLine();
+        int percent =int.Parse(answer);
+
+        string rank = "";
+
+        if (percent>=90)
+        {
+            rank="A";
+        }
+        else if (percent>=80)
+        {
+            rank="B";
+        }
+        else if (percent>=70)
+        {
+            rank="C";
+        }
+        else if (percent >=60)
+        {
+            rank="D";
+        }
+        else if (percent < 60)
+        {
+            rank="F";
+        }
+        else
+        {
+            rank="F";
+        }
+        Console.WriteLine($"Your grade is {rank}");
+        
+        if (percent>=70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time");
+        }
+
+
+
     }
 }
