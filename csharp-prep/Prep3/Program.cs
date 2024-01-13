a714df6965fc2fd37d0ea4e5efc110b97bf58fef
kolawole1248 <kolawoleuthman12@gmail.com>
2024-01-13 02:33:26 +0100

    prep 3 finished

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomgenerator=new Random();
+        int wonderNumber=randomgenerator.Next(1, 101);
+
+        int guess=-1;
+        while(guess != wonderNumber)
+        {
+            Console.Write("What is your guess number ?");
+            guess=int.Parse(Console.ReadLine());
+
+            if(wonderNumber>guess)
+            {
+                Console.WriteLine("Higer");
+            }
+            else if (wonderNumber<guess)
+            {
+                Console.WriteLine("Lower");
+            }
+
+            else
+            {
+                Console.Write("You guess right thank you for playing");
+            }
+
+
+
+
+
+
+
+
+        }
     }
 }
